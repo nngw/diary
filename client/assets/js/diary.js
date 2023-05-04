4a25da90cb1693befe16dede9b7f87d97fc6dc33
christophersharpe <chrismsharpe123@gmail.com>
2023-05-04 17:39:11 +0100

    made create.html diary.html and made homepage for index.html

---
+++ client/assets/js/diary.js
@@ -0,0 +1,12 @@
+function createPost(data) {
+    // Create Elements
+    const container = document.getElementById('entry-container')
+    const title = document.createElement('h5')
+    title.classList.add('card-title')
+    const category = document.createElement('h6')
+    category.classList.add('card-subtitle', 'mb-2', 'text-muted')
+    const content = document.createElement('p')
+    content.classList.add('card-text')
+
+    // Fill elements
+}
