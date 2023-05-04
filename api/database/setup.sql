aba7f70a1a25e5a270c0a8e3b18d90c769860e29
nngw <thanushiyan@hotmail.co.uk>
2023-05-04 16:30:52 +0100

    update SQL entries table

--- api/database/setup.sql
+++ api/database/setup.sql
@@ -1,8 +1,11 @@
 DROP TABLE IF EXISTS entries;
 
 CREATE TABLE entries (
-    id INT GENERATED ALWAYS AS IDENTITY,
-    entry_date VARCHAR(50) NOT NULL,
-    diary_entry VARCHAR(400) NOT NULL,
-    PRIMARY KEY (id)
+    entry_id INT GENERATED ALWAYS AS IDENTITY,
+    entry_date DATE NOT NULL,
+    entry_time TIME NOT NULL,
+    catagory VARCHAR NOT NULL,
+    entry_title VARCHAR(100) NOT NULL,
+    diary_entry VARCHAR(500) NOT NULL,
+    PRIMARY KEY (entry_id)
 );
